ae768f87a067698baf310d0e2ec9b789145fce38
denfort50 <denfort50@yandex.ru>
2022-08-17 18:41:14 +0300

    5. Список Вакансий [#504842]

---
+++ src/main/java/ru/job4j/dream/store/CandidateStore.java
@@ -0,0 +1,35 @@
+package ru.job4j.dream.store;
+
+import ru.job4j.dream.model.Candidate;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CandidateStore {
+
+    private static final CandidateStore INST = new CandidateStore();
+
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+
+    private CandidateStore() {
+        candidates.put(1, new Candidate(1,
+                "Tomas Anderson",
+                "Junior Java Developer")
+        );
+        candidates.put(2, new Candidate(2,
+                "Neo",
+                "Middle Java Developer"));
+        candidates.put(3, new Candidate(3,
+                "John Wick",
+                "Senior Java Developer"));
+    }
+
+    public static CandidateStore instOf() {
+        return INST;
+    }
+
+    public Collection<Candidate> findAll() {
+        return candidates.values();
+    }
+}
