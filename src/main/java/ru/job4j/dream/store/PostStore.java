f47abac9db9d3cca056cd6b21ab6f5d4d18af437
denfort50 <denfort50@yandex.ru>
2022-08-23 08:45:39 +0300

    3. PostController.createPost. Создание вакансии. [#504849]

--- src/main/java/ru/job4j/dream/store/PostStore.java
+++ src/main/java/ru/job4j/dream/store/PostStore.java
@@ -32,4 +32,8 @@ public class PostStore {
     public Collection<Post> findAll() {
         return posts.values();
     }
+
+    public Post add(Post post) {
+        return posts.put(post.getId(), post);
+    }
 }
