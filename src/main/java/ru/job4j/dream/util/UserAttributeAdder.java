6e84a57280bd23e98fad8955c7a97072d39c7435
denfort50 <denfort50@yandex.ru>
2022-10-01 09:52:59 +0300

    2. HttpSession [#504864 #347406]

--- src/main/java/ru/job4j/dream/util/UserAttributeAdder.java
+++ src/main/java/ru/job4j/dream/util/UserAttributeAdder.java
@@ -1,10 +1,15 @@
-package ru.job4j.dream.controller;
+package ru.job4j.dream.util;
 
 import org.springframework.ui.Model;
 import ru.job4j.dream.model.User;
 import javax.servlet.http.HttpSession;
 
 public final class UserAttributeAdder {
+
+    private UserAttributeAdder() {
+        throw new java.lang.UnsupportedOperationException("Utility class and cannot be instantiated");
+    }
+
     public static void addAttributeUser(Model model, HttpSession session) {
         User user = (User) session.getAttribute("user");
         if (user == null) {
