9cc55d012a5db63e57646f171f34d5d99c9aaa42
denfort50 <denfort50@yandex.ru>
2022-10-02 11:05:19 +0300

    3. Filter [#504865 #347763]

--- src/main/java/ru/job4j/dream/filter/AuthFilter.java
+++ src/main/java/ru/job4j/dream/filter/AuthFilter.java
@@ -6,9 +6,23 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Component
 public class AuthFilter implements Filter {
+
+    private final Set<String> mappings = new HashSet<>();
+
+    public AuthFilter() {
+        mappings.add("loginPage");
+        mappings.add("login");
+        mappings.add("formAddUser");
+        mappings.add("fail");
+        mappings.add("success");
+        mappings.add("registration");
+    }
 
     @Override
     public void doFilter(
@@ -18,8 +32,7 @@ public class AuthFilter implements Filter {
         HttpServletRequest req = (HttpServletRequest) request;
         HttpServletResponse res = (HttpServletResponse) response;
         String uri = req.getRequestURI();
-        if (uri.endsWith("loginPage") || uri.endsWith("login") || uri.endsWith("formAddUser")
-                || uri.endsWith("fail") || uri.endsWith("success") || uri.endsWith("registration")) {
+        if (mappingIsPresent(uri)) {
             chain.doFilter(req, res);
             return;
         }
@@ -28,5 +41,16 @@ public class AuthFilter implements Filter {
             return;
         }
         chain.doFilter(req, res);
+    }
+
+    private boolean mappingIsPresent(String uri) {
+        boolean result = false;
+        for (String mapping : mappings) {
+            if (uri.endsWith(mapping)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 }
